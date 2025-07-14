614b43e3dd696ac64a602c0ad8b697c637771863
Nathan Barber <nathanbarber@o11.lan>
2025-07-13 23:06:37 -0400

    Simplify socket class for different modes

--- src/socket/socket.rs
+++ src/socket/socket.rs
@@ -1,7 +1,4 @@
-use crate::{
-    consts,
-    queue::{recv_queue::RecvQueue, send_queue::SendQueue},
-};
+use crate::consts;
 use std::{
     collections::HashMap,
     error::Error,
@@ -23,15 +20,7 @@ pub enum SockState {
     Disconnected,
 }
 
-#[derive(PartialEq, Eq)]
-pub enum PeerType {
-    Inbound,
-    Outbound,
-}
-
 pub struct Peer {
-    pub peer_type: PeerType,
-
     pub bytes_sent: usize,
     pub bytes_recv: usize,
     pub last_seen: Option<Instant>,
@@ -40,19 +29,12 @@ pub struct Peer {
 }
 
 impl Peer {
-    pub fn new(peer_type: PeerType) -> Self {
-        let last_connect = match peer_type {
-            PeerType::Inbound => None,
-            PeerType::Outbound => Some(Instant::now()),
-        };
-
+    pub fn new() -> Self {
         Self {
-            peer_type,
-
             bytes_sent: 0,
             bytes_recv: 0,
             last_seen: None,
-            last_connect,
+            last_connect: None,
             failures: 0,
         }
     }
@@ -60,11 +42,13 @@ impl Peer {
 
 pub struct Socket {
     sock: UdpSocket,
+
     pub mode: SockMode,
     pub state: SockState,
     pub created_at: Instant,
 
-    peers: HashMap<SocketAddr, Peer>,
+    outbound_peer: Option<(SocketAddr, Peer)>,
+    inbound_peers: Option<HashMap<SocketAddr, Peer>>,
 }
 
 impl Socket {
@@ -75,7 +59,8 @@ impl Socket {
             state: SockState::Bound,
             created_at: Instant::now(),
 
-            peers: HashMap::new(),
+            outbound_peer: None,
+            inbound_peers: Some(HashMap::new()),
         })
     }
 
@@ -86,10 +71,9 @@ impl Socket {
             Err(_) => SockState::Disconnected,
         };
 
-        let mut peers = HashMap::new();
+        let mut peer = Peer::new();
+        peer.last_connect = Some(Instant::now());
         let peer_addr = SocketAddr::from_str(addr)?;
-
-        peers.insert(peer_addr, Peer::new(PeerType::Outbound));
 
         Ok(Socket {
             sock,
@@ -97,68 +81,94 @@ impl Socket {
             state,
             created_at: Instant::now(),
 
-            peers,
+            outbound_peer: Some((peer_addr, peer)),
+            inbound_peers: None,
         })
     }
 
     pub fn reconnect(&mut self) -> Result<(), Box<dyn Error>> {
-        if self.mode == SockMode::Bind {
-            return Ok(());
-        }
+        if let (SockMode::Connect, SockState::Disconnected) = (&self.mode, &self.state) {
+            let Some((peer_addr, peer)) = &mut self.outbound_peer else {
+                return Err("connect mode socket has no peer".into());
+            };
 
-        if self.state == SockState::Connected {
-            return Ok(());
-        }
+            let now = Instant::now();
+            let last_connect = peer.last_connect.unwrap_or(now.clone());
+            let time_since_last_connect = now.duration_since(last_connect).as_secs_f64();
 
-        for (peer_addr, peer) in self.peers.iter_mut() {
-            if peer.peer_type == PeerType::Outbound {
-                if let Some(last_connect) = peer.last_connect {
-                    let time_since_last_connect =
-                        Instant::now().duration_since(last_connect).as_secs_f64();
-                    if time_since_last_connect < consts::PEER_RECONNECT_WAIT {
-                        break;
-                    }
+            if time_since_last_connect < consts::PEER_RECONNECT_WAIT {
+                return Ok(());
+            }
+
+            peer.last_connect = Some(now);
+
+            match self.sock.connect(*peer_addr) {
+                Ok(()) => {
+                    self.state = SockState::Connected;
+                    return Ok(());
                 }
-
-                match self.sock.connect(peer_addr) {
-                    Ok(()) => {
-                        self.state = SockState::Connected;
-                    }
-                    Err(e) => {
-                        return Err(Box::new(e));
-                    }
+                Err(e) => {
+                    return Err(Box::new(e));
                 }
-
-                break;
             }
         }
 
-        Ok(())
+        return Ok(());
     }
 
-    pub fn recv(&mut self) -> Result<Vec<u8>, Box<dyn Error>> {
+    pub fn recv(&mut self) -> Result<(SocketAddr, Vec<u8>), Box<dyn Error>> {
         self.reconnect()?;
 
         let mut buffer = vec![0u8; consts::MAX_FRAME_SIZE];
-
         let (bytes_recv, peer) = self.sock.recv_from(&mut buffer)?;
-        let peer = self
-            .peers
-            .entry(peer)
-            .or_insert(Peer::new(PeerType::Inbound));
-        peer.bytes_recv += bytes_recv;
-        peer.last_seen = Some(Instant::now());
+
+        match self.mode {
+            SockMode::Connect => {
+                if let Some((_, peer)) = &mut self.outbound_peer {
+                    peer.bytes_recv += bytes_recv;
+                    peer.last_seen = Some(Instant::now());
+                }
+            }
+            SockMode::Bind => {
+                if let Some(peers) = &mut self.inbound_peers {
+                    let peer = peers.entry(peer).or_insert(Peer::new());
+                    peer.bytes_recv += bytes_recv;
+                    peer.last_seen = Some(Instant::now());
+                }
+            }
+        }
 
         buffer.truncate(bytes_recv);
 
-        Ok(buffer)
+        Ok((peer, buffer))
     }
 
     pub fn send_peer(&mut self, peer_addr: &SocketAddr, data: &[u8]) -> Result<(), Box<dyn Error>> {
         self.reconnect()?;
 
-        let Some(peer) = self.peers.get_mut(peer_addr) else {
-            return Err("no such peer".into());
+        let peer = match self.mode {
+            SockMode::Connect => {
+                if let Some((outbound_peer_addr, peer)) = &mut self.outbound_peer {
+                    if outbound_peer_addr == peer_addr {
+                        peer
+                    } else {
+                        return Err("no such peer".into());
+                    }
+                } else {
+                    return Err("no peer".into());
+                }
+            }
+            SockMode::Bind => {
+                if let Some(peers) = &mut self.inbound_peers {
+                    if let Some(peer) = peers.get_mut(peer_addr) {
+                        peer
+                    } else {
+                        return Err("no such peer".into());
+                    }
+                } else {
+                    return Err("no peers".into());
+                }
+            }
         };
 
         match self.sock.send_to(data, peer_addr) {
